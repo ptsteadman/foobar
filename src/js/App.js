ad81c905781cfed6ca194e27451a3c3efe68f0d9
Patrick Steadman <ptsteadman@gmail.com>
2016-07-01 11:27:38 -0400

    add basic fetch

--- src/js/App.js
+++ src/js/App.js
@@ -1,7 +1,37 @@
 import React from 'react';
 
+const API_BASE = 'http://api-3283.iheart.com/api/v1/catalog/searchAll?keywords=';
+const API_PARAMS = '&queryTrack=false&queryBundle=false&queryArtist=true&' +
+  'queryStation=false&queryFeaturedStation=false&queryTalkShow=false&' +
+  'queryTalkTheme=false&queryKeyword=false&countryCode=US';
+
 const App = React.createClass({
-  render() {
+  getInitialState: function () {
+    return {
+      searchTerm: '',
+      artists: [],
+      error: false,
+    };
+  },
+  getArtists: function () {
+    fetch(API_BASE + this.state.searchTerm + API_PARAMS , {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+      },
+    }).then(function(response){
+      if (response.status === 200) return response.json();
+      var error = new Error(response.statusText);
+      error.response = response;
+      throw error;
+    }).then(function(data){
+      self.setState({ artist: data });
+    }).catch(function(error){
+      console.log(error);
+      self.setState({ error : true });
+    });
+  },
+  render: function () {
     return (
       <h1>App</h1>
     );
