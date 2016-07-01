0e4d14250fea67bf4f12699ec0b0cca4dfec8db7
Patrick Steadman <ptsteadman@gmail.com>
2016-07-01 11:40:21 -0400

    add searchTerm prop to SearchForm

--- src/js/App.js
+++ src/js/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from './Header';
 import ArtistList from './ArtistList';
 import SearchForm from './SearchForm';
-import ArtistList from './ArtistList';
 
 const css = require('!style!css!sass!../sass/styles.scss');
 
@@ -48,6 +47,7 @@ const App = React.createClass({
       <div className="wrapper">
         <Header />
         <SearchForm
+          searchTerm={this.state.searchTerm}
           handleInput={this.onSearchInput}
           handleSubmit={this.onSearchSubmit} 
         />
--- src/js/SearchForm.js
+++ src/js/SearchForm.js
@@ -3,13 +3,14 @@ import React from 'react';
 function SearchForm(props) {
   return (
     <div>
-      <input type="text" />
+      <input value={props.searchTerm} type="text" />
       <input type="submit" />
     </div>
   );
 }
 
 SearchForm.propTypes = {
+  searchTerm: React.PropTypes.string.isRequired,
   handleInput: React.PropTypes.func.isRequired,
   handleSubmit: React.PropTypes.func.isRequired,
 };
