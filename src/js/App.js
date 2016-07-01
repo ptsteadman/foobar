3d46b3107ea86ea26e094019550630843ef64bfc
Patrick Steadman <ptsteadman@gmail.com>
2016-07-01 12:42:46 -0400

    refactor fetch with es6

--- src/js/App.js
+++ src/js/App.js
@@ -7,46 +7,45 @@ const css = require('!style!css!sass!../sass/styles.scss');
 
 const API_BASE = 'http://api-3283.iheart.com/api/v1/catalog/searchAll?keywords=';
 const API_PARAMS = '&queryTrack=false&queryBundle=false&queryArtist=true&' +
-  'queryStation=false&queryFeaturedStation=false&queryTalkShow=false&' +
-  'queryTalkTheme=false&queryKeyword=false&countryCode=US';
+                   'queryStation=false&queryFeaturedStation=false&queryTalkShow=false&' +
+                   'queryTalkTheme=false&queryKeyword=false&countryCode=US';
 
 const App = React.createClass({
-  getInitialState: function () {
+  getInitialState() {
     return {
       searchTerm: '',
       artists: [],
       error: false,
     };
   },
-  getArtists: function () {
-    const self = this;
+  getArtists() {
     fetch(API_BASE + encodeURI(this.state.searchTerm) + API_PARAMS, {
       method: 'GET',
       headers: {
         'Accept': 'application/json',
       },
-    }).then(function(response){
+    }).then((response) => {
       if (response.status === 200) return response.json();
       let error = new Error(response.statusText);
       error.response = response;
       throw error;
-    }).then(function(data){
-      self.setState({ artists: data.artists });
-    }).catch(function(error){
+    }).then((data) => {
+      this.setState({ artists: data.artists });
+    }).catch((error) => {
       console.log(error);
-      self.setState({ error : true });
+      this.setState({ error : true });
     });
   },
-  onSearchInput: function (e) {
+  onSearchInput(e) {
     this.setState({
       searchTerm: e.target.value,
     });
   },
-  onSearchSubmit: function (e) {
+  onSearchSubmit(e) {
     e.preventDefault();
     this.getArtists();
   },
-  render: function () {
+  render() {
     return (
       <div className="wrapper">
         <Header />
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 //  Index boilerplate that loads the bundle
 require('file?name=[name].[ext]!../index.html');
 
-// JavaScript Modules
 import React from  'react';
 import ReactDOM from 'react-dom';
 import 'whatwg-fetch'; // fetch polyfill for AJAX
