340a68da906647ab1bce874e99d6227c71756522
Zachary Taylor <TaylorZach@prahs.com>
2016-07-01 12:16:40 -0400

    style artist list

--- src/js/Artist.js
+++ src/js/Artist.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 function Artist(props) {
   return (
-    <div className="artist">
-      <div className="container">
-        <div className="col-md-4 col-sm-6">
-          <img src="" className="artist__image" />
-          <h3 className="artist__name">{props.name}</h3>
-          <p className="artist__description">Artist description</p>
-        </div>
+    <div className="artist col-md-4 col-sm-6">
+      <div className="artist__inner">
+        <img src="" className="artist__image" />
+        <h3 className="artist__name">{props.name}</h3>
+        <p className="artist__description">Artist description</p>
       </div>
     </div> 
   );
